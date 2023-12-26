5c9ce7b8304ab3e1324758a3ddfa0c570c7a9107
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-12-26 20:32:04 +0530

    StackOverFlow - Updated User Repository

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,4 +1,26 @@
 package repository;
 
+import model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserRepository {
+    private Map<Integer, User> userMap;
+
+    public UserRepository() {
+        this.userMap = new HashMap<>();
+    }
+
+    public User get(int userId) {
+        User user = userMap.get(userId);
+        if (user == null) {
+            throw new UserNotFoundException("User not found for : " + userId);
+        }
+        return user;
+    }
+
+    public void put(User user) {
+        userMap.put(user.getUserId(), user);
+    }
 }
