13d28ccf9ad31324d2f290174d3c2965d168b7d5
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-12-27 21:45:52 +0530

    StackOverFlow - Created the Question Repository

---
+++ src/repository/QuestionRepository.java
@@ -0,0 +1,26 @@
+package repository;
+
+import model.Question;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionRepository {
+    private Map<Integer, Question> questionMap;
+
+    public QuestionRepository() {
+        this.questionMap = new HashMap<>();
+    }
+
+    public Question get(int questionId) {
+        Question question = questionMap.get(questionId);
+        if (question == null) {
+            // throw new QuestionNotFoundException("Question not found for : " + questionId);
+        }
+        return question;
+    }
+
+    public void put(Question question) {
+        questionMap.put(question.getQuestionId(), question);
+    }
+}
